9e7df8577e7231658e0df33d82e815c755721e8c
jacky.li <jacky.li@accenture.com>
2018-04-15 11:25:48 +0800

    add web-chat-demo-client base on vue

---
+++ example/web-chat-demo-client/src/components/Layout.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="body">
+    <div class="header">
+      <Info />
+    </div>
+    <div class="main"></div>
+    <div class="footer"></div>
+  </section>
+</template>
+
+<script>
+  import Info from './Info';
+  export default {
+    data (){
+      return {};
+    },
+    components : {
+      Info
+    }
+  }
+</script>
+
+<style>
+  .body{
+    background: #eee;
+    width: 980px;
+    margin: 0 auto;
+  }
+  .header{
+    min-height: 80px;
+
+  }
+</style>
