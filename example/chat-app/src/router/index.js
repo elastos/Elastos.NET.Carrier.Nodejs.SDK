aa8ba22e4e279942aee73d64a42f3a0265f269e5
jacky.li <jacky.li@accenture.com>
2018-05-02 15:16:39 +0800

    rename file

--- example/chat-app/src/router/index.js
+++ example/chat-app/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/layout/Home';
 import Login from '@/layout/Login';
-import Setting from '@/layout/Setting';
+import Setting from '@/layout/SettingPage';
 
 Vue.use(Router);
 
